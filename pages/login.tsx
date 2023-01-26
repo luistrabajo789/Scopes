0b419e181b75d3c317d76dce6f05f23fd68676eb
luis <luismzo789@gmail.com>
2023-01-26 08:20:29 -0500

    remove google

--- pages/login.tsx
+++ pages/login.tsx
@@ -93,15 +93,15 @@ export default function Login() {
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300"></div>
                 </div>
-                <div className="relative flex justify-center text-sm">
+                {/* <div className="relative flex justify-center text-sm">
                   <span className="px-2 text-neutral-600 bg-white">
                     {" "}
                     Or continue with{" "}
                   </span>
-                </div>
+                </div> */}
               </div>
               <div>
-                <button
+                {/* <button
                   onClick={() => {
                     setLoading(true);
                     signIn("google");
@@ -148,7 +148,7 @@ export default function Login() {
                     </svg>
                     <span className="ml-4"> Log in with Google</span>
                   </div>
-                </button>
+                </button> */}
               </div>
             </form>
           </div>

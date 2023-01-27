9e34108b48335e09aec29060d7da2b6e0a28f6c0
luis <luismzo789@gmail.com>
2023-01-26 21:05:37 -0500

    remove component productcard

--- components/cards/ProductCard.tsx
+++ components/cards/ProductCard.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { ObjectCardProducts } from 'utils/dataPages/dataProducts'
+// import { ObjectCardProducts } from 'utils/dataPages/dataProducts'
 
-export default function ProductCard({image,titleCardProduct,descriptionCard,category,price,link}:ObjectCardProducts) {
+export default function ProductCard({image,titleCardProduct,descriptionCard,category,price,link}:any ) {
   return (
     <div className="col-span-3 w-full">
-          <Link href='#' className="block relative h-48 rounded overflow-hidden">
+          {/* <Link href='#' className="block relative h-48 rounded overflow-hidden">
             <Image width={100} height={100} alt={titleCardProduct} className="object-cover object-center  block" src={image}/>
           </Link>
           <div className="mt-4">
             <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{category}</h3>
             <h2 className="text-gray-900 title-font text-lg font-medium">{titleCardProduct}</h2>
             <p className="mt-1">${price}</p>
-          </div>
+          </div> */}
         </div>
   )
 }
